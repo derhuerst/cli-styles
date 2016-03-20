c890f4cb3d7b59e00972af9bb55df1d03319f087
Jannis R <mail@jannisr.de>
2016-03-20 16:09:59 +0100

    implementation :clap:

---
+++ index.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const chalk =   require('chalk')
+const figures = require('figures')
+
+
+
+// For showing the user's input.
+const styles = Object.freeze({
+	password:  (input) => '*'.repeat(input.length),
+	invisible: (input) => '',
+	default:   (input) => input
+})
+const render = (type) => styles[type] || styles.default
+
+// An icon to signalize a prompt.
+const symbols = Object.freeze({
+	aborted: chalk.red(figures.cross),
+	done:    chalk.green(figures.tick),
+	default: chalk.cyan('?')
+})
+const symbol = (done, aborted) =>
+	aborted ? symbols.aborted : (done ? symbols.done : symbols.default)
+
+// Between the question and the user's input.
+const delimiter = chalk.gray('>')
+
+
+
+module.exports = Object.freeze({
+	styles,
+	render,
+	symbols,
+	symbol,
+	delimiter
+})
