2f9910ef2b5a82ed9b0e88a37ad6565c99db4982
Jannis R <mail@jannisr.de>
2016-03-22 03:56:20 +0100

    `pointerSmall` as prompt delimiter

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const symbol = (done, aborted) =>
 	aborted ? symbols.aborted : (done ? symbols.done : symbols.default)
 
 // Between the question and the user's input.
-const delimiter = chalk.gray('>')
+const delimiter = chalk.gray(figures.pointerSmall)
 
 
 
